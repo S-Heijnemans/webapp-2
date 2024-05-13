8d24cbe14141360edbac4704401112bfa01bf933
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-05-13 14:39:56 +0200

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body> 
+    <div class="container">
+        <div class="test">
+            <div class="orb"></div>
+        </div>
+        <div class="test-1">
+            <div class="orb"></div>
+        </div>
+        <div class="test-2">
+            <div class="orb"></div>
+        </div>
+        <div class="test-3">
+            <div class="orb"></div>
+        </div>
+        <div class="test-4">
+            <div class="orb"></div>
+        </div>
+    </div>\
+</body>
+</html> 
