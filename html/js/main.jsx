cc45c5e84f641284eeabd0ede38528081264a02d
Maik Hendriks <maikew2006@gmail.com>
2024-05-17 13:13:59 +0200

    Fix for bounce

--- html/js/main.jsx
+++ html/js/main.jsx
@@ -1,11 +1,12 @@
+const titel = document.querySelector('.titel');
 const warning = () => {
-    const titel = document.querySelector('.titel');
+
     titel.className = 'warning';
     titel.textContent = 'Pas op! je hebt de titel veranderd';
     console.log(titel.textContent);
 }
 const yeet = () => {
-    const titel = document.querySelector('.titel');
+    
     const bounce = Math.floor(Math.random() * 100);
     titel.style = `margin-top: ${bounce}px`;
 }
