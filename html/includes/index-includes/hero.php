109dc1ddf0087d2d8dd24334a4f9eb4a3f9fb7c8
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-05-31 14:43:22 +0200

    hero blok index
    ik heb de hero blok voor de index gemaakt

---
+++ html/includes/index-includes/hero.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div class="hero-box-index">
+<div class="container">
+    <div class="info-hero-box-index">
+
+        <div class="info-hero-box-top-index">
+            <div class="title-hero-box-index">
+                <H1>Explore the world</H1>
+            </div>
+            <div class="style-line-hero-box-index"></div>
+            <div class="text-hero-box-index">
+                <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis interdum</a> 
+                <a>Nunc nec ipsum magna. Donec sollicitudin gravida porta. Donec molestie gravida laoreet.</a>
+                <a>Nam non vestibulum purus. Ut suscipit libero ac vehicula suscipitporta consequat. Curabitur.</a>
+            </div>
+        </div>
+        <div class="info-hero-box-bottom-index">
+            <div class="buttuns-top-hero-box-index">
+                <div class="destinations-button-hero-index">
+                    <h3>Destinations</h3>
+                </div>
+                <div class="bookAFlight-button-hero-index">
+                    <h3>Book a flight</h3>
+                </div>
+            </div>
+            <div class="buttuns-bottum-hero-box-index">
+                <div class="bottum-button-hero-index">
+                    <a>Fast</a>
+                </div>
+                <div class="bottum-button-hero-index">
+                    <a>Cheap</a>
+                </div>
+                <div class="bottum-button-hero-index">
+                    <a>Quality</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="image-hero-box-index">
+        <img src="../../assets/Images-right-hero-section.png" alt="image not loading">
+    </div>
+
+</div>
+</div>
+</body>
+</html>
--- html/index.php
+++ html/index.php
@@ -9,11 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;900&display=swap" rel="stylesheet">
      
 </head>
 <body> 
 <?php 
     include "includes/header.php";
+    include "includes/index-includes/hero.php";
 ?>
      
     <div class="loadingbox">
