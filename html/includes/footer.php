f8cdde77c90ba41edd6f7100a9d1094fde5c011f
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-06-20 08:37:34 +0200

    footer
    footer

---
+++ html/includes/footer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container-footer">
+
+        <div class="icon-container-footer">
+
+            <div class="icon-box-footer">
+            <img src="../../assets/airplane-footer.png" alt="image not loading">
+                <div class="text-icon-box-footer">
+                    <h3>Flights & Trips</h3>
+                    <h5>Flights</h5>
+                    <h5>Trips</h5>
+                </div>
+            </div>
+
+            <div class="icon-box-footer">
+            <img src="../../assets/chat-square-text-footer.png" alt="image not loading">
+                <div class="text-icon-box-footer">
+                    <h3>Contact support</h3>
+                    <h5>Contact@gmail.com</h5>
+                    <h5>+31 6 1234 5678</h5>
+                </div>
+            </div>
+
+            <div class="icon-box-footer">
+            <img src="../../assets/building-footer.png" alt="image not loading">
+                <div class="text-icon-box-footer">
+                    <h3>Booked flights</h3>
+                    <h5>Your flights</h5>
+                    <h5>Continue payments</h5>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="style-line-footer"></div>
+
+        <div class="info-cotainer-footer">
+            <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque consequat nunc vestibulum</a>
+            <a>porttitor.Nunc mattis metus at interdum egestas. Nullam malesuada felis et tortor</a>
+            <a>congue, sed convallis nisl bibendum.</a>
+        </div>
+
+        <div class="style-line-footer"></div>
+
+        <div class="nav-container-footer">
+            <div class="title-text-nav-footer">
+                <h1> </h1>
+            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
