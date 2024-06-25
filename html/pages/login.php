32adbdb9dac4e7be734f83d7dc9c602ef2c39248
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2024-06-25 22:19:00 +0200

    header stylen

--- html/pages/login.php
+++ html/pages/login.php
@@ -11,18 +11,28 @@
     include "../includes/header.php";
 ?>
 
-<div class='container'>
-        <div class='block-login'>
-              <form class='form-login-tabel' action="../includes/login-includes/login_logic.php" name='login_logic' method="POST">
-                    <label>Username:</label>
-                    <input type="text" name='username' placeholder="Username" required>
-                    <label>Password:</label>
-                    <input type="password" name='password' placeholder="Password" required>
-                    <input type="submit" value="Login">
-                </form>
-                <a href='register.php'>register</a>
+
+<div class='login-container'>
+    <div class='form-container'>
+        <div class="form-header">
+            <button class="active">Login</button>
+            <button>Sign up</button>
         </div>
+        <form class='form-login-tabel' action="../includes/login-includes/login_logic.php" name='login_logic'
+              method="post">
+            <label>Username:</label>
+            <input type="text" name='username' placeholder="Username" required>
+            <label>Password:</label>
+            <input type="password" name='password' placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+        <a href='register.php'>register</a>
+    </div>
+    <div class="image-container">
+        <img class="img-login" src="../assets/img-login.png" alt="">
+    </div>
 </div>
+
 <?php 
     include "../includes/footer.php";
 ?>
