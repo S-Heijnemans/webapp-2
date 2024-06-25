e199646259569428b4e46b2d243ecc3c6cd5e192
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-06-25 11:27:56 +0200

    searchbar css

--- html/includes/trips-includes/searchbar.php
+++ html/includes/trips-includes/searchbar.php
@@ -34,7 +34,7 @@ try {
 <div class="search-for-flight-box">
     <form class="search_for_flight" action="../../pages/trips.php" method="post">
 
-        <select name="starting_location" id="starting_location" required onchange="disableSameDestination()">
+        <select class="search-select-box-container" name="starting_location" id="starting_location" required onchange="disableSameDestination()">
             <option value='' disabled selected>Start location</option>
             <?php
             foreach ($results as $row) {
@@ -46,7 +46,7 @@ try {
 
             ?>
         </select>
-        <select name="destination" id="destination" required>
+        <select class="search-select-box-container" name="destination" id="destination" required>
             <option value='' disabled selected>Destination</option>
             <?php
             foreach ($results as $row) {
@@ -56,9 +56,9 @@ try {
                 echo '</option>';
             } ?>
         </select>
-        <input type="date" name="start_date" id="startdate" required value="">
-        <input type="date" name="end_date" id="enddate" required onfocus="this.showPicker()" value="">
-        <button type="submit">Submit</button>
+        <input class="search-select-box-container" type="date" name="start_date" id="startdate" required value="">
+        <input class="search-select-box-container" type="date" name="end_date" id="enddate" required onfocus="this.showPicker()" value="">
+        <button class="search-button-box" type="submit">Submit</button>
     </form>
     <form class="reset-query" action="../../pages/trips.php">
         <button type="submit" id="reset-query-button">RESET filter</button>
